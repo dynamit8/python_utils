da487fcd2a9ad30ad6ce6a37ecf7fd86880b856b
Wisanu Upatumphun <dynamit8@gmail.com>
2021-03-17 11:13:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def sublistByNum(ls, result, num):
         nextsub = ls[num:]
         return sublistByNum(nextsub, result, num)
     return result
+
+
+from datetime import datetime
+# https://www.w3schools.com/python/python_datetime.asp
+now = datetime.now()
+# Turn obj to str
+nowStr = now.strftime("%Y%m%d-%H-%M-%S")
+# Turn str to obj
+nowObject = datetime.strptime(nowStr, "%Y%m%d-%H-%M-%S")
